e3276ae7431ece5d24aeb48a89734c4c979edd24
alekseii <dorogazm@gmail.com>
2023-09-26 15:54:15 +0300

    20.2 Templating

--- catalog/views.py
+++ catalog/views.py
@@ -23,3 +23,9 @@ def contacts(request):
         visiter["message"] = request.POST.get('message', None)
         print(visiter)
     return render(request, 'catalog/contacts.html', context=context)
+
+
+def detail_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_detail.html', context=context)
