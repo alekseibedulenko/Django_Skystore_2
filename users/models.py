7e891401306022a26a9d91f9339f0e7d707a1068
alekseii <dorogazm@gmail.com>
2023-10-13 14:01:30 +0300

    Fixed the input

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
                                verbose_name='Аватар')
     phone = models.CharField(max_length=35, verbose_name='Телефон')
     country = models.CharField(max_length=35, verbose_name='Страна')
-    is_verified_email = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
